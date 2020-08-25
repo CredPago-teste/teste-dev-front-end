d25123eda59bbbbad9d818fc9a100c0cf920e539
Rodrigo <rodrigo@yogh.com.br>
2020-08-25 18:37:38 -0300

    Implementa estrutura da página de Login

--- implementacao/src/pages/SignIn/index.tsx
+++ implementacao/src/pages/SignIn/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Container, LoginBox } from './styles';
+
+import logoImg from '../../assets/images/logo-credpago.svg';
 
 const SignIn: React.FC = () => {
   return (
-    <h1>Login</h1>
+    <Container>
+      <img src={logoImg} alt="CredPago"/>
+      <LoginBox>
+        <form>
+          <div>
+            <label>
+              Email: <span>*</span>
+            </label>
+            <input type="email" placeholder="seu@email.com"/>
+          </div>
+          <div>
+            <label>
+              Senha: <span>*</span>
+            </label>
+            <input type="password" placeholder="*******"/>
+          </div>
+          <div>
+            <button type="submit">Entrar</button>
+          </div>
+          <div>
+            <a href="/">Esqueci minha senha</a>
+          </div>
+        </form>
+      </LoginBox>
+    </Container>
   );
 }
 
